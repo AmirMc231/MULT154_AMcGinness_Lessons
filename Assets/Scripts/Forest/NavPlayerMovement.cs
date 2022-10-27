41a1095a9e71d3a03452ba5dc421204408a19aaa
AmirMc231 <amirmc231@gmail.com>
2022-10-27 07:26:10 -0700

    Unity AI

---
+++ Assets/Scripts/Forest/NavPlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavPlayerMovement : MonoBehaviour
+{
+    public float speed = 40.0f;
+    public float rotationSpeed = 100.0f;
+    Rigidbody rgBody = null;
+    float trans = 0;
+    float rotate = 0;
+
+    public delegate void DropHive(Vector3 pos);
+    public static event DropHive DroppedHive;
+
+    private void Start()
+    {
+        rgBody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DroppedHive?.Invoke(transform.position + (transform.forward * 10));
+        }
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+    }
+}
